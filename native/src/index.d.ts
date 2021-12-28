e16f332cc023fc4b7b00f64d0e289362b9000b44
Anders Gerner <github@andersgerner.dk>
2021-12-28 16:01:47 +0100

    Removed audioplayer

--- native/src/index.d.ts
+++ native/src/index.d.ts
@@ -1,2 +1,1 @@
-export { default as AudioPlayer } from "./components/AudioPlayer";
 export { default as Icon } from "./components/Icon";
--- native/src/index.tsx
+++ native/src/index.tsx
@@ -1,3 +1,2 @@
-export { default as AudioPlayer } from "./components/AudioPlayer";
 export { default as Icon } from "./components/Icon";
 export { default as WebView } from "./components/WebView";
